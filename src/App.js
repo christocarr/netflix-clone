097fb90a0a1703730d828fefd85255c4849554d8
Chris <christocarr@outlook.com>
2020-09-01 15:15:52 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React from 'react';
-
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 
 function App() {
   return (
-    <div>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/signin">
+          <p>Sign in page</p>
+        </Route>
+        <Route path="/signup">
+          <p>Sign up page</p>
+        </Route>
+        <Route path="/browse">
+          <p>Browse page</p>
+        </Route>
+        <Route path="/">
+          <p>Home page</p>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
