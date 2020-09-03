f4f5d197624cfeb91000214334c1eeadc8646137
christocarr <christocarr@outlook.com>
2020-09-03 09:09:24 +0100

    Add header feature

--- src/pages/home.js
+++ src/pages/home.js
@@ -4,16 +4,21 @@ import {JumbotronContainer} from '../containers/jumbotron'
 import {FaqsContainer} from '../containers/faq';
 import {FooterContainer} from '../containers/footer'
 import OptForm from '../components/opt-form'
+import Feature from '../components/feature'
 
 export default function Home() {
     return (
         <>
             <HeaderContainer>
-                <OptForm>
-                    <OptForm.Input placeholder="Email address" />
-                    <OptForm.Button>Try Now</OptForm.Button>
-                    <OptForm.Text>Ready to watch? Enter your email to create or restart your membership.</OptForm.Text>
-                </OptForm>
+                <Feature>
+                    <Feature.Title>Unlimited films, TV programmes and more.</Feature.Title>
+                    <Feature.Subtitle>Watch anywhere. Cancel at any time.</Feature.Subtitle>
+                    <OptForm>
+                        <OptForm.Input placeholder="Email address" />
+                        <OptForm.Button>Try Now</OptForm.Button>
+                        <OptForm.Text>Ready to watch? Enter your email to create or restart your membership.</OptForm.Text>
+                    </OptForm>
+                </Feature>
             </HeaderContainer>
             <JumbotronContainer />
             <FaqsContainer />
