8333ca248274ef72aa0d990322503f01220d869c
christocarr <christocarr@outlook.com>
2020-09-04 09:17:35 +0100

    Add custom hook to get firestore data

--- src/pages/browse.js
+++ src/pages/browse.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import BrowseContainer from '../containers/browse'
+import useContent from '../hooks/use-content'
 
 export default function Browse() {
+  const {films} = useContent('films')
+  const {series} = useContent('series')
   return <BrowseContainer />
 }
