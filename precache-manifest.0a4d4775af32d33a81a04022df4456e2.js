1e0d19996b23c91d76fef41dd1d746a6dd3e3c88
christocarr <christocarr@outlook.com>
2020-09-07 18:26:38 +0100

    Updates

---
+++ precache-manifest.0a4d4775af32d33a81a04022df4456e2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5734750b58f93d4361aef960dd3a8007",
+    "url": "/netflix-clone/index.html"
+  },
+  {
+    "revision": "132a41040d155e62c65c",
+    "url": "/netflix-clone/static/js/2.863186bf.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/netflix-clone/static/js/2.863186bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e348f55751516c902f3c",
+    "url": "/netflix-clone/static/js/main.6906ed4d.chunk.js"
+  },
+  {
+    "revision": "42ccba1c2e183f5632cb",
+    "url": "/netflix-clone/static/js/runtime-main.baddefd0.js"
+  }
+]);
